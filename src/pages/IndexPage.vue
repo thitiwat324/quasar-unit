5704ed684fe18d1a054221ba41e3ec7ed641c0fd
thitiwat324 <mesih2453@gmail.com>
2023-10-13 21:09:30 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'ฐิติวัสส์ เพชรแทน',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
